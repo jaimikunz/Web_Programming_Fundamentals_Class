be6e53fd47e3ec1c167388c1dd385a3c3a5cc13c
jaimi kunz <jaimikunz@gmail.com>
2013-07-17 15:10:25 -0500

    pop up

---
+++ Associatingjs/script.js
@@ -0,0 +1,4 @@
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height - prompt("Please enter the height.");
+var area = width * height;
+console.log(area);
