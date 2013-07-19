a5c435372e7c70f8f4fae81123d74dedfa314e02
jaimi kunz <jaimikunz@gmail.com>
2013-07-18 22:33:05 -0500

    Expressions_Personal

---
+++ Expressions/Expressions_Industry/Wachniak_jaimi_w2_07:18:2013.js
@@ -0,0 +1,7 @@
+var avgdiapers1 = 2500;
+var avgdiapers2 = 2000;
+var avgdiapers3 = 1860;
+var avgdiapers4 = 1900;
+
+var average = (avgdiapers1 + avgdiapers2 + avgdiapers3 + avgdiapers4)/4;
+console.log(average);
