e55ec029e4c416cc15c5a079036aaf94eecd9588
jaimi kunz <jaimikunz@gmail.com>
2013-07-25 15:17:37 -0500

    ternarie

---
+++ Conditionals/Conditionals_industry/js/Ternarie1.js
@@ -0,0 +1,8 @@
+var gpa = 48;
+if( gpa > 2.0){
+	console.log ("you can graduate!");
+}else{
+	console.log ("gpa is to low!")
+}
+
+( gpa > 2.0) ? console.log("You can Graduate!") : console.log("GPA Too Low!")
