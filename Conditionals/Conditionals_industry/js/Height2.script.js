ffa0d20d612b944ab967e9f047ce0441e36a390a
jaimi kunz <jaimikunz@gmail.com>
2013-07-25 15:14:00 -0500

    conditionals

---
+++ Conditionals/Conditionals_industry/js/Height2.script.js
@@ -0,0 +1,9 @@
+var kidHeight =52;
+var minHeight =48;
+var wParentHeight =45;
+
+console.log("you can ride!");
+}else if(kidHeight < wParentHeight){
+	console.log("you can ride but only with you parent!");
+} else{ console.log("sorry kid, you have got some growing to do!");
+}
