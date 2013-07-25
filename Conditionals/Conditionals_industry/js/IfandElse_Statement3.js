d57049523dbb69fbc8819d3aeb70c01593f7a0dd
jaimi kunz <jaimikunz@gmail.com>
2013-07-25 15:14:36 -0500

    if and else

---
+++ Conditionals/Conditionals_industry/js/IfandElse_Statement3.js
@@ -0,0 +1,8 @@
+var kidWeight = 50;
+var minWeight = 45;
+if(kidWeight > minWeight){
+	console.log("you can ride in the front seat!");
+
+}else{
+console.log("Sorry, you have to grow up a little more!");
+}
