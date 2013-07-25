9dadb17c8a75c115120d796b813bdda84516a789
jaimi kunz <jaimikunz@gmail.com>
2013-07-25 15:15:22 -0500

    logical operators

---
+++ Conditionals/Conditionals_industry/js/Lotto.js
@@ -0,0 +1,9 @@
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+if(iPhonePrice < budget || wonLottery){
+	console.log("we can buy the phone!");
+}else{
+	console.log("No phone for you!!")
+}
