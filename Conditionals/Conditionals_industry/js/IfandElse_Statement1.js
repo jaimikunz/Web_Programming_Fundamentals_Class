1ae3f133a50b8b5ed5c410e7decf86347037d347
jaimi kunz <jaimikunz@gmail.com>
2013-07-25 15:14:11 -0500

    if and else

---
+++ Conditionals/Conditionals_industry/js/IfandElse_Statement1.js
@@ -0,0 +1,8 @@
+var kidHeight = 47;
+var minHeight = 48;
+if(kidHeight > minHeight){
+	console.log("you can ride the hayride!");
+
+}else{
+console.log("Sorry, you have to grow a couple more inches!");
+}
