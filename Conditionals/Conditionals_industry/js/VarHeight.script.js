49c4f20601343985dbb9614c27e862124e55a405
jaimi kunz <jaimikunz@gmail.com>
2013-07-25 15:18:41 -0500

    coditional

---
+++ Conditionals/Conditionals_industry/js/VarHeight.script.js
@@ -0,0 +1,10 @@
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2 ;
+
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift> minHeight){
+	//code performed if conditiom is true
+	console.log("You can ride the coster!")
+}
