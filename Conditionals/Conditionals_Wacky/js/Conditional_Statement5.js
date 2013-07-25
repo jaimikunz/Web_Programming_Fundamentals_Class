cee727917bc2a6ec8fd853cc7121908ea2b5d744
jaimi kunz <jaimikunz@gmail.com>
2013-07-25 15:13:44 -0500

    conditional statement

---
+++ Conditionals/Conditionals_Wacky/js/Conditional_Statement5.js
@@ -0,0 +1,11 @@
+if(sunny)}else{goToTheMovies();}
+
+if(temp>70){goToTheBeach();}
+
+if (temp ==75){goToTheBeach();}
+
+if(sunny){goToTheBeach();} else{if(warmWater){wearNewSuit();
+} goToTheMovies();
+} 
+
+console.log("goToTheBeach!")
