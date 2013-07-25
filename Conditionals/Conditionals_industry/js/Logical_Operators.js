32de6f0cf07c34c33ddc233794a7fc939b84269a
jaimi kunz <jaimikunz@gmail.com>
2013-07-25 15:15:40 -0500

    logical operators

---
+++ Conditionals/Conditionals_industry/js/Logical_Operators.js
@@ -0,0 +1,9 @@
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
+
+if(iPhonePrice && paycheck > 300){
+	console.log("we can buy the phone!");
+}else{
+	console.log("No phone for you!!")
+}
