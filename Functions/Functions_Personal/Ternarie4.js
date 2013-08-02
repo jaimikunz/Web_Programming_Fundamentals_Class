da5b80e52e21c739bf1c8d480279785ec51a4f72
jaimi kunz <jaimikunz@gmail.com>
2013-08-01 21:35:06 -0500

    Jaimi_Wachniak_08/01/2013_Ternanies

---
+++ Functions/Functions_Personal/Ternarie4.js
@@ -0,0 +1,8 @@
+var temp = 75;
+if( temp > 65){
+	console.log ("go to the park!");
+}else{
+	console.log ("better stay inside")
+}
+
+( temp > 65) ? console.log("go to the park") : console.log("better stay inside")
