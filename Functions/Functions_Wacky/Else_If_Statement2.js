adcf63d877a3fca12dd89a637275b3740aa3809e
jaimi kunz <jaimikunz@gmail.com>
2013-08-01 21:27:53 -0500

    Jaimi_Wachniak_08/01/2013_Else_If_Statements

---
+++ Functions/Functions_Wacky/Else_If_Statement2.js
@@ -0,0 +1,8 @@
+var kidWeight = 37;
+var minWeight = 45;
+if(kidWeight > minWeight){
+	console.log("Your big enough can ride the Go Carts!");
+
+}else{
+console.log("Sorry, try again next year!");
+}
