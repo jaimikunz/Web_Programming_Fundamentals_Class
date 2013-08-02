c52e3ec0a6bd48cf12763ddfda3bcaa3b13455c0
jaimi kunz <jaimikunz@gmail.com>
2013-08-01 21:30:34 -0500

    Jaimi_Wachniak_08/01/2013_Logical_Operators

---
+++ Functions/Functions_Wacky/Logical_Operator2.js
@@ -0,0 +1,9 @@
+var budget = 2000;
+var vacationPrice = 2100;
+var paycheck = 1000;
+
+if(vacationPrice && paycheck > 2000){
+	console.log("we can go to hawaii!");
+}else{
+	console.log("Nope, wait another month!!")
+}
