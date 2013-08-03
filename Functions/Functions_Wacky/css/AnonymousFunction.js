3f4e8a08d2508bd2ae2038bbb12a295c435dd935
jaimi kunz <jaimikunz@gmail.com>
2013-08-03 18:16:03 -0500

    commit

---
+++ Functions/Functions_Wacky/css/AnonymousFunction.js
@@ -0,0 +1,8 @@
+var calcArea = function(width, height){
+	var area = width * height;
+	return area;
+
+}
+var a = calcArea(40,20);
+console.log(a);
+
---
+++ Functions/Functions_Wacky/css/AnonymousFunction2.js
@@ -0,0 +1,8 @@
+var calcArea = function(w, h){
+	var area = w * h;
+	return area;
+
+}
+var a = calcArea(60,40);
+console.log(a);
+
