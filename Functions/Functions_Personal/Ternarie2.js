15e8ae5b99e2458468c8ac9e3b8de3ee8b455348
Jaimi Kunz <jaimikunz@gmail.com>
2013-08-03 18:03:54 -0500

    Delete Ternarie2.js

--- Functions/Functions_Personal/Ternarie2.js
+++
@@ -1,8 +0,0 @@
-var gpa = 3.2;
-if( gpa > 1.5){
-	console.log ("you can graduate!");
-}else{
-	console.log ("gpa is to low!")
-}
-
-( gpa > 1.5) ? console.log("You can Graduate!") : console.log("GPA Too Low!")
