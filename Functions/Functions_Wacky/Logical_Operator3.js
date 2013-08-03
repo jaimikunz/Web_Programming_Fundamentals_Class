5276f08713f8fe3547da2abee9397196810ec30e
Jaimi Kunz <jaimikunz@gmail.com>
2013-08-03 18:03:21 -0500

    Delete Logical_Operator3.js

--- Functions/Functions_Wacky/Logical_Operator3.js
+++
@@ -1,9 +0,0 @@
-var budget = 700;
-var computerPrice = 650;
-var paycheck = 800;
-
-if(computerPrice && paycheck > 800){
-	console.log("go get that computer!");
-}else{
-	console.log("sorry next paycheck")
-}
