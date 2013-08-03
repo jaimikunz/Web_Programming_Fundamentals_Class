55937fad23c955f78892659299c2c4ab32c68f72
Jaimi Kunz <jaimikunz@gmail.com>
2013-08-03 18:04:09 -0500

    Delete Ternarie4.js

--- Functions/Functions_Personal/Ternarie4.js
+++
@@ -1,8 +0,0 @@
-var temp = 75;
-if( temp > 65){
-	console.log ("go to the park!");
-}else{
-	console.log ("better stay inside")
-}
-
-( temp > 65) ? console.log("go to the park") : console.log("better stay inside")
