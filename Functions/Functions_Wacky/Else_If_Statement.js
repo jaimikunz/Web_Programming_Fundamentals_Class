6930b7258321dce2a337b958012a2e930d3d4452
Jaimi Kunz <jaimikunz@gmail.com>
2013-08-03 18:02:38 -0500

    Delete Else_If_Statement.js

--- Functions/Functions_Wacky/Else_If_Statement.js
+++
@@ -1,8 +0,0 @@
-var kidHeight = 37;
-var minHeight = 45;
-if(kidHeight > minHeight){
-	console.log("you can ride the hayride to the apple orchard!");
-
-}else{
-console.log("Sorry, you need to ride with your parents!");
-}
