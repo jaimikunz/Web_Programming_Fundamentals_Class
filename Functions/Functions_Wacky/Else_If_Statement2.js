4f89f01faa1456492421fdf24c8997e39d56fd79
Jaimi Kunz <jaimikunz@gmail.com>
2013-08-03 18:02:48 -0500

    Delete Else_If_Statement2.js

--- Functions/Functions_Wacky/Else_If_Statement2.js
+++
@@ -1,8 +0,0 @@
-var kidWeight = 37;
-var minWeight = 45;
-if(kidWeight > minWeight){
-	console.log("Your big enough can ride the Go Carts!");
-
-}else{
-console.log("Sorry, try again next year!");
-}
