3469d5a38cf17686f7ec7c14d9f064027c963487
Jaimi Kunz <jaimikunz@gmail.com>
2013-08-03 18:03:15 -0500

    Delete Logical_Operator2.js

--- Functions/Functions_Wacky/Logical_Operator2.js
+++
@@ -1,9 +0,0 @@
-var budget = 2000;
-var vacationPrice = 2100;
-var paycheck = 1000;
-
-if(vacationPrice && paycheck > 2000){
-	console.log("we can go to hawaii!");
-}else{
-	console.log("Nope, wait another month!!")
-}
