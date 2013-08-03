dd90a18203d4e8ebb7753ca73577a4b8c25663be
jaimi kunz <jaimikunz@gmail.com>
2013-08-03 18:14:17 -0500

    commit

---
+++ Functions/Functions_Industry/js/Parameters6.js
@@ -0,0 +1,9 @@
+function lizzardYears(age){
+	var age = 1;
+	var lizzardYears = age * 2;
+	console.log("Pascel is " + lizzardYears + " years old.")
+}
+lizzardYears(4);
+lizzardYears(2);
+lizzardYears(5);
+
